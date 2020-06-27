8650943ef27f4f7510102338a8ae586b01c79e4e
EC2 Default User <ec2-user@ip-172-31-40-194.ap-northeast-1.compute.internal>
2020-06-27 13:25:42 +0000

    ルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "top#index"
+  resources :users, only:[:show, :edit, :update]
+  resources :topics
+  resources :infos
+  resources :pics do
+    resources :comments, only:[:create]
+  end
 end
