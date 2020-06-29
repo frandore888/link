c3719f0a1fbee0bc52b171110b9bd1ce222a1c00
EC2 Default User <ec2-user@ip-172-31-40-194.ap-northeast-1.compute.internal>
2020-06-29 02:03:49 +0000

    picsコントローラーに各アクションを設定

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,2 +1,27 @@
 class PicsController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def show
+  end
+  
+  private
+  def create_params
+  end
+  
+  def update_params
+  end
+  
 end
