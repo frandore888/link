f4d17066078fc00a8aef01620e457d1a74824daa
EC2 Default User <ec2-user@ip-172-31-40-194.ap-northeast-1.compute.internal>
2020-06-29 01:58:11 +0000

    infos, topicsコントローラーにアクションを設定

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,2 +1,26 @@
 class InfosController < ApplicationController
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  def create_params
+  end
+  
+  def update_params
+  end
 end
